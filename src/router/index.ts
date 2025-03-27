95ba52795c68ce6b2d99753c3cf1d49175f09bd8
yikousu <yikousu369@gmail.com>
2025-03-27 16:36:12 +0800

    fix: 解决刷新出现404问题。改用hash路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import qqqqq from "@/components/AboutMe.vue";
 import SnakeGame from "@/components/SnakeGame.vue";
 import SearchComponent from "@/components/SearchComponent.vue";
@@ -44,8 +44,7 @@ let routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
-
 export default router;
